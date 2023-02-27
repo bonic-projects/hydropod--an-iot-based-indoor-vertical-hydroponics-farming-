a72a55302a5e123e82be38858af143ba0565338b
mohdazhar <mohdazharsm@gmail.com>
2023-02-27 13:32:26 +0530

    Datatype conversion ph value error fixed

--- hydropod_hydroponics/lib/models/models.dart
+++ hydropod_hydroponics/lib/models/models.dart
@@ -18,7 +18,9 @@ class DeviceData {
 
   factory DeviceData.fromMap(Map data) {
     return DeviceData(
-      ph: data['ph'] ?? 0,
+      ph: data['ph'] != null
+          ? (data['ph'] % 1 == 0 ? data['ph'] + 0.1 : data['ph'])
+          : 0,
       waterFLow: data['flowRate'] ?? 0,
       totalWaterFlow: data['totalMilliLitres'] ?? 0,
       waterLevel: data['wtr_level'] ?? 0,
